8c9751836bf1b0a5f8c27622f16a855c4bdcd887
Jean-Bernard Ratte <jean.bernard.ratte@gmail.com>
2015-08-03 21:26:10 -0400

    Added information an version handling for python

--- setup.py
+++ setup.py
@@ -1,16 +1,34 @@
 __author__ = 'Jean-Bernard Ratte - jean.bernard.ratte@unary.ca'
+__version__ = '0.0.1'
+
+import os
+import sys
 from setuptools import setup, find_packages
+
+
+if sys.version_info[:2] < (2, 7):
+    raise RuntimeError('pyjarowinkler requires Python 2.7 minimum (untested on python 3)')
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup_info = {
     'name': 'plexcleaner',
-    'version': '0.1.0',
+    'version': __version__,
     'maintainer': __author__.split(' - ')[0],
     'maintainer_email': __author__.split(' - ')[1],
     'author': __author__.split(' - ')[0],
     'author_email': __author__.split(' - ')[1],
-    'url': 'http://{0}'.format(__author__.split('@')[1]),
+    'url': 'https://github.com/nap/plexcleaner',
+    'download_url': "https://github.com/nap/plexcleaner/archive/v{0}.zip".format(__version__),
     'license': 'http://www.apache.org/licenses/',
+    'description': 'PlexCleaner will read your Plex Database and create a new library with '
+                   'updated filename and directory structure including movie jacket of matched media.',
+    'platforms': ['Linux'],
+    'keywords': 'plex library movie media matching taglib jacket',
     'packages': find_packages(),
+    'long_description': read('README.rst'),
     'classifiers': [
         'Development Status :: 2 - Pre-Alpha',
         'Environment :: Console',
@@ -20,9 +38,6 @@ setup_info = {
         'Programming Language :: Python :: 2.7',
         'Topic :: Multimedia :: Video',
         'Topic :: Utilities',
-    ],
-    'entry_points': {
-        'console_scripts': ['plexcleaner=plexcleaner:main']
-    }
+    ]
 }
 setup(**setup_info)
