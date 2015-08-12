cbdaeb276bb49e9f83e03120c536cded8970148b
Jean-Bernard Ratte <jean.bernard.ratte@gmail.com>
2015-08-11 20:55:30 -0400

    added check if plex app is running when trying to update db

--- plexcleaner/cleaner.py
+++ plexcleaner/cleaner.py
@@ -3,6 +3,8 @@ import click
 import logging
 import os
 import shutil
+import signal
+import errno
 
 import cli
 from plexcleaner import LOG
@@ -16,6 +18,29 @@ def get_free_fs_space(export):
     statvfs = os.statvfs(export)
     avail_size = statvfs.f_frsize * statvfs.f_bavail
     return avail_size * 1024
+
+
+def is_plex_running(pid_file='/var/run/PlexMediaServer.pid'):
+    try:
+        with open(pid_file) as pf:
+            pid = int(pf.read())
+            os.kill(pid, signal.SIG_DFL)
+
+    except OSError as e:
+        if e.errno == errno.ESRCH:
+            return False
+
+        elif e.errno == errno.EPERM:
+            return True  # The process does not have permission to send the signal to any of the target processes
+
+        else:
+            raise PlexOSException('Unable to validate if Plex is running')
+
+    except IOError:
+        return False
+
+    else:
+        return True
 
 
 @click.command()
@@ -46,8 +71,9 @@ def main(plex_home, export, update, jacket, interrupt, log_level, database_overr
                 LOG.critical('Not enough space: {0} Bytes > {1} Bytes'.format(library.effective_size, free_space))
                 raise PlexOSException('Remaining space on the target filesystem is not enough to export the library')
 
-        if update:
-            LOG.info('Will update media file location in Plex Database')
+        if update and is_plex_running():
+            LOG.critical('Cannot update media file location if Plex is running')
+            raise PlexOSException('Should not update database if Plex is running')
 
         for movie in library:
             LOG.info(u"Processing: '{0}'".format(movie.basename))
