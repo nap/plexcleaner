9268cda4f7947971e541cddb8295fb8febbd40a0
Jean-Bernard Ratte <jean.bernard.ratte@gmail.com>
2015-08-12 22:56:45 -0400

    removed extra exception

--- plexcleaner/exception.py
+++ plexcleaner/exception.py
@@ -1,21 +1,13 @@
-from plexcleaner import LOG
-
+from plexcleaner import LOG, log_severity
 __author__ = 'Jean-Bernard Ratte - jean.bernard.ratte@unary.ca'
 
 
 class PlexCleanerException(Exception):
-    def __init__(self, message, **kwargs):
-        super(Exception, self).__init__(message, **kwargs)
-        LOG.debug(message)
+    def __init__(self, *args, **kwargs):
+        super(Exception, self).__init__(args[0])
 
+        log = LOG.debug
+        if 'severity' in kwargs and kwargs['severity'] in log_severity:
+            log = getattr(LOG, log_severity[kwargs['severity']])
 
-class PlexDatabaseException(PlexCleanerException):
-    pass
-
-
-class PlexMediaFileException(PlexCleanerException):
-    pass
-
-
-class PlexOSException(PlexCleanerException):
-    pass
+        log(args[0])
