5fc95e6813a478fdda0d95ef95a05abdfe4085fc
Jean-Bernard Ratte <jean.bernard.ratte@gmail.com>
2015-08-15 17:55:31 -0400

    moved db connection

--- test/test_media_library.py
+++ test/test_media_library.py
@@ -10,15 +10,14 @@ class TestMediaLibrary(unittest.TestCase):
     _nb_movie = 98
     _effective_size = 100275991932
 
-    def setUp(self):
-        self.db = database.Database(database_override='./test/database/com.plexapp.plugins.library.db')
-
     def test_init(self):
-        library = Library(self.db)
+        db = database.Database(database_override='./test/database/com.plexapp.plugins.library.db')
+        library = Library(db)
         self.assertEqual(len(library), self._nb_movie)
 
     def test_update_library(self):
-        library = Library(self.db)
+        db = database.Database(database_override='./test/database/com.plexapp.plugins.library.db')
+        library = Library(db)
         movie = Movie(1, u"a", "b", 1, 2, 2.2, "d", "e")
         library._update_library(movie)
         self.assertEqual(len(library), self._nb_movie + 1)
@@ -29,7 +28,8 @@ class TestMediaLibrary(unittest.TestCase):
     #     self.assertTrue('Could not connect' in e.exception.message)
 
     def test_effective_size(self):
-        library = Library(self.db)
+        db = database.Database(database_override='./test/database/com.plexapp.plugins.library.db')
+        library = Library(db)
         movie = Movie(2, u"a", 'b', 1, 2, 2.2, 'd', 'e')
         movie.exist = False
         library._update_library(movie)
@@ -40,16 +40,19 @@ class TestMediaLibrary(unittest.TestCase):
         self.assertEqual(library.effective_size, self._effective_size + 2)
 
     def test_iter(self):
-        library = Library(self.db)
+        db = database.Database(database_override='./test/database/com.plexapp.plugins.library.db')
+        library = Library(db)
         self.assertEqual(type(library.__iter__()).__name__, 'generator')
         m = library.__iter__().next()
         self.assertEqual(m.__class__.__name__, 'Movie')
 
     def test_has_missing_file(self):
-        library = Library(self.db)
+        db = database.Database(database_override='./test/database/com.plexapp.plugins.library.db')
+        library = Library(db)
         self.assertFalse(library.has_missing_file)
         os.rename('test/library/2 Guns.avi', 'test/library/Two Guns.avi')
-        library = Library(self.db)
+        db = database.Database(database_override='./test/database/com.plexapp.plugins.library.db')
+        library = Library(db)
         self.assertTrue(library.has_missing_file)
         os.rename('test/library/Two Guns.avi', 'test/library/2 Guns.avi')
 
