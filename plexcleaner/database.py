e7fe86d7df6fb97f64879aa183b2b647313ad7a9
Jean-Bernard Ratte <jean.bernard.ratte@gmail.com>
2015-08-15 17:40:50 -0400

    removed database logic from class

---
+++ plexcleaner/database.py
@@ -0,0 +1,39 @@
+import sqlite3
+import os
+import logging
+from plexcleaner import LOG
+from exception import PlexCleanerException
+
+__author__ = 'Jean-Bernard Ratte - jean.bernard.ratte@unary.ca'
+
+
+class Database(object):
+    _database_path = 'Library/Application Support/Plex Media Server/Plug-in Support/Databases'
+    _update_movie = "UPDATE media_parts SET media_parts.file = '{0}' WHERE media_parts.id = '{1}'"
+    _select_movies = (
+        'SELECT media_parts.id, metadata_items.title, media_parts.file, metadata_items.year, ',
+        'media_parts.size, media_items.frames_per_second AS fps, '
+        'metadata_items.guid, metadata_items.user_thumb_url AS jacket FROM media_items ',
+        'JOIN metadata_items ON media_items.metadata_item_id = metadata_items.id ',
+        'JOIN media_parts ON media_parts.media_item_id = media_items.id'
+    )
+
+    def __init__(self, metadata_home='/var/lib/plexmediaserver',
+                 database_override=None, database_name='com.plexapp.plugins.library.db'):
+        database = os.path.join(metadata_home, self._database_path, database_name)
+        try:
+            if database_override:
+                database = database_override
+
+            with sqlite3.connect(database) as conn:
+                self.cursor = conn.cursor()
+
+        except sqlite3.OperationalError:
+            raise PlexCleanerException("Could not connect to Plex database: {0}".format(database),
+                                       severity=logging.WARNING)
+
+    def get_rows(self):
+        return self.cursor.execute(''.join(self._select_movies))
+
+    def update_row(self, mid, value):
+        self.cursor.execute(self._update_movie.format(mid, value))
