e5a2952bc09245f27092d358cb735408568ec949
Jean-Bernard Ratte <jean.bernard.ratte@gmail.com>
2015-08-16 12:06:52 -0400

    added test for database

--- test/test_database.py
+++ test/test_database.py
@@ -1,6 +1,13 @@
 import unittest
+from plexcleaner.exception import PlexCleanerException
+from plexcleaner.database import Database
+
 __author__ = 'Jean-Bernard Ratte - jean.bernard.ratte@unary.ca'
 
 
 class TestDatabase(unittest.TestCase):
-    pass
+
+    def test_database_exception(self):
+        with self.assertRaises(PlexCleanerException) as e:
+            db = Database(database_override='./path/to/com.plexapp.plugins.library.db')
+        self.assertTrue('Could not connect' in e.exception.message)
