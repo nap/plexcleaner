96ebb35105457d71e2a94464a57ce1fc7e59e442
Jean-Bernard Ratte <jean.bernard.ratte@gmail.com>
2015-08-16 13:40:54 -0400

    test for updates

--- test/test_database.py
+++ test/test_database.py
@@ -11,3 +11,21 @@ class TestDatabase(unittest.TestCase):
         with self.assertRaises(PlexCleanerException) as e:
             db = Database(database_override='./path/to/com.plexapp.plugins.library.db')
         self.assertTrue('Could not connect' in e.exception.message)
+
+    def test_update(self):
+        db = Database(database_override=':memory:')
+        db._cursor.execute('CREATE TABLE media_parts(id, file)')
+        db._cursor.execute('INSERT INTO media_parts (id, file) VALUES (?, ?)', [1, '/test/fail'])
+        db.update_row(1, '/test/success')
+        row = db._cursor.execute('SELECT file FROM media_parts WHERE id = ?', (1,)).fetchone()
+        self.assertEqual(row[0], '/test/success')
+
+    def test_update_many(self):
+        db = Database(database_override=':memory:')
+        db._cursor.execute('CREATE TABLE media_parts(id, file)')
+        db._cursor.executemany('INSERT INTO media_parts (id, file) VALUES (?, ?)', [(1, '/test/fail'),
+                                                                                    (2, '/test/fail'),
+                                                                                    (3, '/test/fail')])
+        db.update_many_row([('/test/success', 1), ('/test/success', 2), ('/test/success', 3)])
+        for row in db._cursor.execute('SELECT file FROM media_parts').fetchall():
+            self.assertEqual(row[0], '/test/success')
