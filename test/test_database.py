98f878a3b3dffaf05e146c91b8237330a487e803
Jean-Bernard Ratte <jean.bernard.ratte@gmail.com>
2015-08-16 13:48:37 -0400

    added test for getrows

--- test/test_database.py
+++ test/test_database.py
@@ -6,6 +6,13 @@ __author__ = 'Jean-Bernard Ratte - jean.bernard.ratte@unary.ca'
 
 
 class TestDatabase(unittest.TestCase):
+    def get_db(self):
+        db = Database(database_override=':memory:')
+        db._cursor.execute('CREATE TABLE media_parts(id, file)')
+        db._cursor.executemany('INSERT INTO media_parts (id, file) VALUES (?, ?)', [(1, '/test/fail'),
+                                                                                    (2, '/test/fail'),
+                                                                                    (3, '/test/fail')])
+        return db
 
     def test_database_exception(self):
         with self.assertRaises(PlexCleanerException) as e:
@@ -13,19 +20,19 @@ class TestDatabase(unittest.TestCase):
         self.assertTrue('Could not connect' in e.exception.message)
 
     def test_update(self):
-        db = Database(database_override=':memory:')
-        db._cursor.execute('CREATE TABLE media_parts(id, file)')
-        db._cursor.execute('INSERT INTO media_parts (id, file) VALUES (?, ?)', [1, '/test/fail'])
+        db = self.get_db()
         db.update_row(1, '/test/success')
         row = db._cursor.execute('SELECT file FROM media_parts WHERE id = ?', (1,)).fetchone()
         self.assertEqual(row[0], '/test/success')
 
     def test_update_many(self):
-        db = Database(database_override=':memory:')
-        db._cursor.execute('CREATE TABLE media_parts(id, file)')
-        db._cursor.executemany('INSERT INTO media_parts (id, file) VALUES (?, ?)', [(1, '/test/fail'),
-                                                                                    (2, '/test/fail'),
-                                                                                    (3, '/test/fail')])
+        db = self.get_db()
         db.update_many_row([('/test/success', 1), ('/test/success', 2), ('/test/success', 3)])
         for row in db._cursor.execute('SELECT file FROM media_parts').fetchall():
             self.assertEqual(row[0], '/test/success')
+
+    def test_get_rows(self):
+        db = self.get_db()
+        rows = db._cursor.execute('SELECT file FROM media_parts').fetchall()
+        self.assertEqual(len(rows), 3)
+
