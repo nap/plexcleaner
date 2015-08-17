03c6a1b6117fad732d13a2cc012d0f88c106dab0
Jean-Bernard Ratte <jean.bernard.ratte@gmail.com>
2015-08-16 21:45:33 -0400

    added with statement support; update log; update move function

--- plexcleaner/cleaner.py
+++ plexcleaner/cleaner.py
@@ -44,28 +44,26 @@ def is_plex_running(pid_file='/var/run/PlexMediaServer.pid'):
 
 
 def move_media(src, dst):
-    # TODO: Handle unicode
     # TODO: Exceptions
     if not os.path.exists(dst):
         shutil.move(src, dst)
 
-    LOG.info("File {0} already exist, will rename/override.")
+    LOG.info("File '{0}' already exist, will rename/override.".format(src))
 
 
 def copy_jacket(src, dst, skip):
     if not os.path.exists(dst) and not skip:
         shutil.copy(src, dst)
 
-    LOG.info("Jacket {0} already exist, skip.")
+    LOG.info("Jacket '{0}' already exist, skip.".format(src))
 
 
 def create_dir(dst):
-    # TODO: Handle unicode
     # TODO: Exceptions
     if not os.path.isdir(dst):
         os.mkdir(dst)
 
-    LOG.info("Directory {0} already exist, skip.")
+    LOG.info("Directory '{0}' already exist, skip.".format(src))
 
 
 def clean_dir(dst):
@@ -74,10 +72,13 @@ def clean_dir(dst):
     pass
 
 
-def update_database(id, dst):
-    # TODO: Get database instance
-    # TODO: Update row id with dst value
-    pass
+def update_database(db, m, should_update=False):
+    if not should_update:
+        return None
+
+    filename = m.get_correct_absolute_file()
+    db.update_row(m.id, filename)
+    LOG.info("Updating movie '{0}' with path '{1}'".format(m.correct_title, filename))
 
 
 @click.command()
@@ -85,57 +86,61 @@ def update_database(id, dst):
 @click.option('--export', **cli.export)
 @click.option('--update/--no-update', **cli.update)
 @click.option('--jacket', **cli.jacket)
+@click.option('--skip-jacket', **cli.skip_jacket)
 @click.option('--interrupt', **cli.interrupt)
 @click.option('--log-level', **cli.log_level)
 @click.option('--database-override', **cli.database_override)
 def main(plex_home, export, update, jacket, interrupt, log_level, database_override, skip_jacket):
     LOG.setLevel(logging.getLevelName(log_level.upper()))
-    try:
-        db = database.Database(metadata_home=plex_home, database_override=database_override)
-        library = Library(db)
+    with database.Database(metadata_home=plex_home, database_override=database_override) as db:
+        try:
+            library = Library(db)
 
-        if not len(library):
-            raise PlexCleanerException("Library is empty.", severity=logging.WARNING)
+            if not len(library):
+                raise PlexCleanerException("Library is empty.", severity=logging.WARNING)
 
-        if library.has_missing_file and interrupt:
-            raise PlexCleanerException('Missing media file on the filesystem', severity=logging.WARNING)
+            if library.has_missing_file and interrupt:
+                raise PlexCleanerException('Missing media file on the filesystem', severity=logging.WARNING)
 
-        if export:
-            LOG.info("Will consolidate library in: '{0}'".format(export))
+            if export:
+                LOG.info("Will consolidate library in: '{0}'".format(export))
 
-            free_space = get_free_fs_space(export)
-            if library.effective_size > free_space:
-                raise PlexCleanerException('Remaining space on the target filesystem is not enough to export the '
-                                           'library {0} Bytes > {1} Bytes'.format(library.effective_size, free_space),
-                                           severity=logging.CRITICAL)
+                free_space = get_free_fs_space(export)
+                if library.effective_size > free_space:
+                    raise PlexCleanerException('Remaining space on the target filesystem is not enough to export the '
+                                               'library {0} Bytes > {1} Bytes'.format(library.effective_size, free_space),
+                                               severity=logging.CRITICAL)
 
-        if update and is_plex_running():
-            raise PlexCleanerException('Should not update database if Plex is running', severity=logging.ERROR)
+            if update and is_plex_running():
+                raise PlexCleanerException('Should not update database if Plex is running', severity=logging.ERROR)
 
-        for movie in library:
-            LOG.info(u"Processing: '{0}'".format(movie.basename))
-            if movie.matched:
-                try:
-                    create_dir(movie.get_correct_absolute_path(override=export))
-                    # TODO: Delete following line
-                    test = os.path.join('./test/posters', os.path.basename(movie.get_metadata_jacket()))  # FOR TESTING
-                    copy_jacket(test, os.path.join(movie.get_correct_absolute_path(override=export), jacket), skip_jacket)
-                    move_media(movie.original_file, movie.get_correct_absolute_file(override=export))
-                    update_database(movie.id, movie.get_correct_absolute_file(override=export))
+            for movie in library:
+                LOG.info(u"Processing: '{0}'".format(movie.basename))
 
-                except Exception:  # TODO: Validate exception case
-                    # TODO: log...
-                    clean_dir(movie.get_correct_absolute_path(override=export))
+                if movie.matched:
+                    try:
+                        create_dir(movie.get_correct_absolute_path(override=export))
+                        # TODO: Delete following line
+                        test = os.path.join('./test/posters', os.path.basename(movie.get_metadata_jacket()))  # FOR TESTING
+                        copy_jacket(test, os.path.join(movie.get_correct_absolute_path(override=export), jacket), skip_jacket)
+                        move_media(movie.original_file, movie.get_correct_absolute_file(override=export))
 
-            else:
-                LOG.info("Movie '{0}' was not matched in Plex".format(movie.basename))
+                        # TODO: Update media_items.hits, will want to commit by batch, not by items
+                        update_database(db, movie, should_update=update)
 
-    except PlexCleanerException as ce:
-        print ce.message
+                    except Exception:  # TODO: Validate exception case
+                        # TODO: log...
+                        clean_dir(movie.get_correct_absolute_path(override=export))
 
-    except KeyboardInterrupt:
-        LOG.info("bye.")
-        sys.exit(0)
+                else:
+                    LOG.info("Movie '{0}' was not matched in Plex".format(movie.basename))
+
+        except PlexCleanerException as ce:
+            print ce.message
+
+        except KeyboardInterrupt:
+            LOG.info("bye.")
+            sys.exit(0)
 
 if __name__ == '__main__':
     main()
