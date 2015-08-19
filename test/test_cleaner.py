dbafef40d19acee602a3a008977687ad2c9262ec
Jean-Bernard Ratte <jean.bernard.ratte@gmail.com>
2015-08-18 21:17:56 -0400

    added signature of methode to test

--- test/test_cleaner.py
+++ test/test_cleaner.py
@@ -4,3 +4,12 @@ __author__ = 'Jean-Bernard Ratte - jean.bernard.ratte@unary.ca'
 
 class TestCleaner(unittest.TestCase):
     pass
+    #def get_free_fs_space(export):
+
+    #def is_plex_running(pid_file='/var/run/PlexMediaServer.pid'):
+
+    #def log_error(err, dst):
+
+    #def move_media(src, dst):
+
+    #def copy_jacket(src, dst, skip):
