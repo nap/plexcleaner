35f0ba6d2cfcf66d5b89659dd68727df28d76747
Jean-Bernard Ratte <jean.bernard.ratte@gmail.com>
2015-08-18 21:22:15 -0400

    added test method

--- test/test_cleaner.py
+++ test/test_cleaner.py
@@ -3,13 +3,38 @@ __author__ = 'Jean-Bernard Ratte - jean.bernard.ratte@unary.ca'
 
 
 class TestCleaner(unittest.TestCase):
-    pass
-    #def get_free_fs_space(export):
+    def test_get_free_fs_space(self):
+        #def get_free_fs_space(export):
+        pass
 
-    #def is_plex_running(pid_file='/var/run/PlexMediaServer.pid'):
+    def test_is_plex_running(self):
+        #def is_plex_running(pid_file='/var/run/PlexMediaServer.pid'):
+        pass
 
-    #def log_error(err, dst):
+    def test_log_error(self):
+        #def log_error(err, dst):
+        pass
 
-    #def move_media(src, dst):
+    def test_move_media(self):
+        #def move_media(src, dst):
+        pass
 
-    #def copy_jacket(src, dst, skip):
+    def test_copy_jacket(self):
+        #def copy_jacket(src, dst, skip):
+        pass
+
+    def test_copy_jacket_skpi(self):
+        #def copy_jacket(src, dst, skip):
+        pass
+
+    def test_creatE_dir(self):
+        #def create_dir(dst):
+        pass
+
+    def test_update_database(self):
+        #def update_database(db, m, should_update=False):
+        pass
+
+    def test_update_database_no_update(self):
+        #def update_database(db, m, should_update=False):
+        pass
