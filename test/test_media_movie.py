2e51970b9a8596206eec2cf94c8501b913552c3a
Jean-Bernard Ratte <jean.bernard.ratte@gmail.com>
2015-08-19 20:10:12 -0400

    added test to validate path parsing

--- test/test_media_movie.py
+++ test/test_media_movie.py
@@ -38,6 +38,10 @@ class TestMediaMovie(unittest.TestCase):
         h = hashlib.sha1('c').hexdigest()
         self.assertTrue(h[1:] in movie.get_metadata_jacket())
 
+    def test_get_metadata_jacket_parsing(self):
+        movie = Movie(1, u"a", 'b.avi', 2010, 2, 2.2, 'c', self._jacket)
+        self.assertEqual(14, len(movie.get_metadata_jacket().split('/')))
+
     def test_get_correct_directory(self):
         movie = Movie(1, u"a", 'b.avi', 2010, 2, 2.2, 'c', self._jacket)
         self.assertEqual(movie.get_correct_directory(), "a (2010)")
