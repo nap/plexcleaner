f6e511417f304fc5d06c6c637c9319b68b1bb720
Jean-Bernard Ratte <jean.bernard.ratte@gmail.com>
2015-08-22 21:52:32 -0400

    added test for media move

--- test/test_cleaner.py
+++ test/test_cleaner.py
@@ -74,6 +74,19 @@ class TestCleaner(unittest.TestCase):
         self.assertTrue(os.path.exists('./test/library/2 Guns (2009)/2 Guns.avi'))
         self.assertTrue(moved)
 
+    @log_capture()
+    def test_move_media_exist(self, l):
+        shutil.copy('./test/library/2 Guns.avi', './test/library/abc.avi')
+        cleaner.create_dir('./test/library/2 Guns (2009)')
+        self.assertTrue(os.path.isdir('./test/library/2 Guns (2009)'))
+        moved = cleaner.move_media('./test/library/abc.avi', './test/library/2 Guns (2009)/2 Guns.avi')
+        self.assertIn('already exist', str(l))
+
+    def test_move_media_src_default(self):
+        with self.assertRaises(PlexCleanerException) as e:
+            moved = cleaner.move_media('./test/library/does_not_exist.avi', './test/library/2 Guns (2009)/2 Guns.avi')
+        self.assertTrue('error occurred' in e.exception.message)
+
     def test_copy_jacket(self):
         cleaner.create_dir('./test/library/2 Guns (2009)')
         self.assertTrue(os.path.isdir('./test/library/2 Guns (2009)'))
